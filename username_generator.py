9e9ef79ef9156ef77b62e203c1812fa721f1b79a
Raghu K A <151126855+raghu-kathalagere@users.noreply.github.com>
2025-11-09 21:51:51 +0530

    Update username_generator.py
    Added comments to clarify purpose of variables.

--- username_generator.py
+++ username_generator.py
@@ -1,11 +1,12 @@
-from itertools import product
+from itertools import product    ## Import product to generate all possible combinations
+
 
 def variants(word):
     return [word, word.capitalize(), word.upper()]
 
-names = ['elliot']
-roles = ['alderson', 'hacker']
-separators = ['', '-', '_','@']
+names = ['elliot']    #your target_uesrname
+roles = ['alderson', 'hacker']    #target second_name or their roles or nickname
+separators = ['', '-', '_','@']    #special-char or symbols to place between name and role
 
 with open('usernames.txt', 'w') as f:
     for n, r, sep in product(names, roles, separators):
